fd70618869a0e07fbd3bf4fabcb17c50b4d7854c
Erostate <erostate55@gmail.com>
2023-04-07 00:52:31 +0200

    Modif du SQL

--- db.sql
+++ db.sql
@@ -10,7 +10,9 @@
 -- Table "Repo"
 CREATE Table repo (
     repo_id INT(11) AUTO_INCREMENT,
-    name VARCHAR(50) DEFAULT 'none' NOT NULL,
+    label VARCHAR(50) DEFAULT 'none' NOT NULL,
     namespace VARCHAR(255) DEFAULT 'none' NOT NULL,
+    init ENUM('true','false') DEFAULT 'false',
+    folder TEXT NOT NULL,
     CONSTRAINT pk_repo PRIMARY KEY(repo_id)
 )
